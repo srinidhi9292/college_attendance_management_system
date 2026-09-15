1af72b245cee68de6f5601390dde7c5182bd9d27
lakshmi srinidhi <srinidhipaideti31@gmail.com>
2026-09-15 20:40:11 +0530

    Update settings.py for database and session management
    Added dj_database_url for database configuration and set SESSION_SAVE_EVERY_REQUEST to True.

--- sollege_attendance_system/settings.py
+++ sollege_attendance_system/settings.py
@@ -4,6 +4,7 @@ Django settings for sollege_attendance_system project.
 
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,11 +54,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sollege_attendance_system.wsgi.application'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
